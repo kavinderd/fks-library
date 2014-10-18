f8251e10cd3f2d3d1f156fdb89c032f7ae42ea22
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-10-17 22:13:41 -0700

    add basic member spec persistence

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Member, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context 'persistence' do
+
+    it "creates a record with valid attributes" do
+      expect do
+        Member.create(name: 'First Last', email: 'test@gmail.com')
+      end.to change(Member, :count).by(1)
+    end
+
+    it "does not create a record with invalid attributes" do
+      expect do
+        Member.create(email: 'test@gmail.com')
+      end.to_not change(Member, :count)
+    end
+  end
+
 end
